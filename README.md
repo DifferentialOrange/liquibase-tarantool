8da49b98d29f013ea1f98590061fcbce0c818113
Georgy Moiseev <moiseev.georgii@gmail.com>
2024-03-01 20:22:34 +0300

    driver: connect to Tarantool

--- README.md
+++ README.md
@@ -6,3 +6,23 @@
 mvn package
 ```
 
+## Test
+
+```bash
+tarantool
+```
+
+```lua
+box.cfg{listen = 3301}
+box.schema.user.create('test', {if_not_exists = true})
+box.schema.user.passwd('test', 'test')
+box.schema.user.grant('test', 'execute', 'universe', nil, {if_not_exists = true})
+```
+
+```bash
+mvn exec:java
+```
+
+```
+2.11.2-0-g1bac2d257b
+```
--- src/main/java/liquibase/ext/tarantool/database/TarantoolClientDriver.java
+++ src/main/java/liquibase/ext/tarantool/database/TarantoolClientDriver.java
@@ -1,13 +1,23 @@
 package liquibase.ext.tarantool.database;
 
-/**
- * Hello world!
- *
- */
+import io.tarantool.driver.api.TarantoolClient;
+import io.tarantool.driver.api.TarantoolClientFactory;
+import io.tarantool.driver.api.TarantoolResult;
+import io.tarantool.driver.api.tuple.TarantoolTuple;
+
+import java.util.List;
+
 public class TarantoolClientDriver 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException, java.util.concurrent.ExecutionException
     {
-        System.out.println( "Hello World!" );
+        TarantoolClient<TarantoolTuple, TarantoolResult<TarantoolTuple>> client = TarantoolClientFactory.createClient()
+            .withAddress("localhost", 3301)
+            .withCredentials("test", "test")
+            .build();
+
+        List<?> version = client.eval("return box.info.version").get();
+
+        System.out.println(version.get(0));
     }
 }
